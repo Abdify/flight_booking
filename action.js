22dfaceb2c5610a5ac0585337097d5ef669ab077
Abdify <abdullah.fahimm08@gmail.com>
2021-01-30 10:54:29 +0600

    Comfirmation added

--- action.js
+++ action.js
@@ -1,30 +1,30 @@
-
 let form = document.querySelector('.booking-form');
 
-document.onload = document.querySelector('#fClassInput').value = 0;
-
+let fClassQuantity = 0;
+let eClassQuantity = 0;
 let subTotal = 0;
+
+//Listen to quantity increase and decrease button click
+//Using Event Delegation
 form.addEventListener('click', event => {
     let btn = event.target.id;
     if(btn === 'fClassIncreaseBtn'){
-        subTotal += 150;
-        changeQuantity('fClassInput', true);
+        fClassQuantity = changeQuantity('fClassInput', true);
     }
     else if(btn === 'fClassDecreaseBtn'){
-        subTotal -= 150;
-        changeQuantity('fClassInput', false);
+        fClassQuantity = changeQuantity('fClassInput', false);
     }
     else if(btn === 'eClassIncreaseBtn'){
-        subTotal += 100;
-        changeQuantity('eClassInput', true);
+        eClassQuantity = changeQuantity('eClassInput', true);
     }
     else if(btn === 'eClassDecreaseBtn'){
-        subTotal -= 100;
-        changeQuantity('eClassInput', false);
+        eClassQuantity = changeQuantity('eClassInput', false);
     }
-    
+    //subtotal
+    subTotal = fClassQuantity * 150 + eClassQuantity * 100;
     updateTotal(subTotal);
 });
+
 
 function changeQuantity(ticketType, willIncrease){
     let Input = document.getElementById(ticketType);
@@ -34,8 +34,55 @@ function changeQuantity(ticketType, willIncrease){
     return quantity;
 }
 
+
 function updateTotal(subTotal){
     document.querySelector('#subTotal').innerText = '$' + subTotal;
     document.querySelector('#tax').innerText = '$' + subTotal * 0.1;
-    document.querySelector('#total').innerText = '$' + Number(subTotal + subTotal * 0.1);
+    let total = document.querySelector('#total').innerText = '$' + Number(subTotal + subTotal * 0.1);
+}
+
+
+//Listen to Submit button click
+let submitBtn = document.querySelector('#submitBtn');
+submitBtn.addEventListener('click', () => {
+    //No ticket? Animation
+    let fClassInput = document.querySelector('#fClassInput');
+    let eClassInput = document.querySelector('#eClassInput');
+    if(subTotal < 100){
+        fClassInput.style.border = '2px solid red';
+        eClassInput.style.border = '2px solid red';
+        fClassInput.style.scale = '1.05';
+        eClassInput.style.scale = '1.05';
+
+        setInterval(() => {
+            fClassInput.style.border = '';
+            eClassInput.style.border = ''; 
+            fClassInput.style.scale = '';
+            eClassInput.style.scale = '';
+        }, 2000);
+
+        return;
+    }
+    //Confirm
+    confirmation();
+});
+
+
+//Confirmation modal
+function confirmation() {
+    let confirm = document.querySelector('#confirmation');
+    confirm.style.top = '0px';
+    confirm.style.opacity = '1';
+
+    document.querySelector('.confirm_info').innerHTML = 'Are you sure to book <br> <b>' + fClassQuantity + ' First class Ticket(s)</b> and <b>' + eClassQuantity + ' Economy Ticket(s)</b> for <b>' + total.innerText + '</b>?';
+    let closeBtn = document.querySelectorAll('.close');
+    window.onclick = function(event) {
+        if (event.target == confirm || 
+            event.target == closeBtn[0] || 
+            event.target == closeBtn[1] || 
+            event.target == closeBtn[2]) {
+                confirm.style.top = '';
+                confirm.style.opacity = '';
+        }
+    }
 }
